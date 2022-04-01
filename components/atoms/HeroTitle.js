87d1cf0fdb71087059f0b31900415f70cb5798db
I. Bats <bats@ibats.co>
2022-04-01 22:28:56 +0200

    refactor herotitle atom

--- components/atoms/HeroTitle.js
+++ components/atoms/HeroTitle.js
@@ -1,3 +1,3 @@
 export default function HeroTitle({ children }) {
-  return <h1 className='text-5xl font-brygada'>{children}</h1>;
+  return <h1 className="text-5xl font-brygada">{children}</h1>;
 }
