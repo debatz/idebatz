5d7281bb4d81365756e820886f4f6908f3a3f3d0
I. Bats <bats@ibats.co>
2022-04-02 01:14:11 +0200

    refactor highlight & linkcomponent atoms

--- components/atoms/LinkComponent.js
+++ components/atoms/LinkComponent.js
@@ -1,14 +1,15 @@
-export default function LinkComponent({ children }) {
+export default function LinkComponent({ children, href }) {
   return (
-    <span
-      className="bg-gradient-to-r from-[#36f9f6] to-[#36f9f6]
+    <a
+      className="cursor-ff1 bg-gradient-to-r from-[#36f9f6] to-[#36f9f6]
     bg-no-repeat [background-position:0_88%]
     [background-size:100%_0.05em]
     motion-safe:transition-all motion-safe:duration-200
     hover:[background-size:100%_100%]
     focus:[background-size:100%_100%]"
+      href={href}
     >
       {children} ↗
-    </span>
+    </a>
   );
 }
