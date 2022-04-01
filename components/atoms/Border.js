fdc70a8eaec0c0ca5ef1baa667aedd1b7a7c3779
I. Bats <bats@ibats.co>
2022-04-01 22:03:10 +0200

    add border atom

---
+++ components/atoms/Border.js
@@ -0,0 +1,12 @@
+import { twClass } from '@utils/twClass';
+
+export default function Border({ className }) {
+  return (
+    <div
+      className={twClass(
+        "border-[#182f45] border-b border-dotted",
+        className && `${className}`
+      )}
+    ></div>
+  );
+}
