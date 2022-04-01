0f88585beed43367cf65291c220b7991428eca73
I. Bats <bats@ibats.co>
2022-04-01 23:41:42 +0200

    add nav

---
+++ components/molecules/Nav.js
@@ -0,0 +1,11 @@
+export default function Nav() {
+  return (
+    <nav>
+      <ul className="text-xl text-right font-fira cursor-ff1">
+        <li>C:\home</li>
+        <li>C:\about</li>
+        <li>C:\contact</li>
+      </ul>
+    </nav>
+  );
+}
