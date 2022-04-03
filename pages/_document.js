35100d1648563d7b674fff12d4bc03ad9046a542
I. Bats <bats@ibats.co>
2022-04-03 14:04:29 +0200

    add react-icons & refactor

--- pages/_document.js
+++ pages/_document.js
@@ -2,11 +2,19 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html>
+    <Html className="cursor-ff2">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link href="https://fonts.googleapis.com/css2?family=Brygada+1918:ital,wght@0,400;0,600;0,700;1,400;1,500;1,600;1,700&family=Fira+Code&display=swap" rel="stylesheet" />      </Head>
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Brygada+1918:ital,wght@0,400;0,600;0,700;1,400;1,500;1,600;1,700&family=Fira+Code&display=swap"
+          rel="stylesheet"
+        />{" "}
+      </Head>
       <body>
         <Main />
         <NextScript />
