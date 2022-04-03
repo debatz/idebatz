5de908d1da504410b965e625d226ff53934c482e
I. Bats <bats@ibats.co>
2022-04-03 14:04:07 +0200

    refactor experience & hero

--- components/organisms/Experience.js
+++ components/organisms/Experience.js
@@ -1,3 +1,4 @@
+import Border from "@components/atoms/Border";
 import LinkComponent from "@components/atoms/LinkComponent";
 import Roles from "@components/molecules/Roles";
 import Tech from "@components/molecules/Tech";
@@ -5,28 +6,33 @@ import Container from "@components/templates/Container";
 
 export default function Experience({ props }) {
   return (
-    <Container>
-      <ul className="space-y-6">
-        {props.map((item, index) => {
-          return (
-            <li key={index} className="border-t border-black">
-              <div className="flex justify-between py-4">
-                <div className="space-y-2">
-                  <h1 className="text-5xl font-brygada">
-                    <LinkComponent href={item.href}>{item.title}</LinkComponent>
-                  </h1>
-                  <p className="text-2xl font-brygada">{item.more}</p>
-                  <div>
-                    <Roles props={item.roles} />
-                    <Tech props={item.tech} />
+    <section>
+      <Container>
+        <ul className="space-y-6">
+          {props.map((item, index) => {
+            return (
+              <li key={index} className="border-t border-black">
+                <div className="flex justify-between py-4">
+                  <div className="space-y-2">
+                    <h1 className="text-5xl font-brygada">
+                      <LinkComponent href={item.href}>
+                        {item.title}
+                      </LinkComponent>
+                    </h1>
+                    <p className="text-2xl font-brygada">{item.more}</p>
+                    <div>
+                      <Roles props={item.roles} />
+                      <Tech props={item.tech} />
+                    </div>
                   </div>
+                  <h3 className="text-5xl font-brygada">{item.year}</h3>
                 </div>
-                <h3 className="text-5xl font-brygada">{item.year}</h3>
-              </div>
-            </li>
-          );
-        })}
-      </ul>
-    </Container>
+              </li>
+            );
+          })}
+        </ul>
+        <Border className="mt-6" />
+      </Container>
+    </section>
   );
 }
--- components/organisms/Hero.js
+++ components/organisms/Hero.js
@@ -2,25 +2,28 @@ import HeroTitle from "@components/atoms/HeroTitle";
 import LinkComponent from "@components/atoms/LinkComponent";
 import Nav from "@components/molecules/Nav";
 import Container from "@components/templates/Container";
-import Border from "@components/atoms/Border";
 
 export default function Hero() {
   return (
-    <Container>
-      <div className="flex items-center justify-between">
-        <div className="w-4/5">
-          <HeroTitle>
-            <span className="italic font-semibold">batz:</span> I am a{" "}
-            <span className="italic font-medium">front-end developer</span> with
-            a polymath background. Currently a BScCS student at the{" "}
-            <LinkComponent>University of London</LinkComponent>. Former digital
-            marketer & startup founder.
-          </HeroTitle>
+    <section>
+      <Container>
+        <div className="flex items-center justify-between">
+          <div className="w-4/5">
+            <HeroTitle>
+              <span className="italic font-semibold">batz:</span> I am a{" "}
+              <span className="italic font-medium">front-end developer</span>{" "}
+              with a polymath background. Currently a BScCS student at the{" "}
+              <LinkComponent href="https://london.ac.uk/courses/computer-science">
+                University of London
+              </LinkComponent>
+              . Former digital marketer & startup founder.
+            </HeroTitle>
+          </div>
+          <div className="w-1/5">
+            <Nav />
+          </div>
         </div>
-        <div className="w-1/5">
-          <Nav />
-        </div>
-      </div>
-    </Container>
+      </Container>
+    </section>
   );
 }
