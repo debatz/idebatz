b6caa87d861d012d36e10c84d085011d9dc06e6d
I. Bats <bats@ibats.co>
2022-04-03 17:59:24 +0200

    refactor spacing in roles & tech

--- components/molecules/Roles.js
+++ components/molecules/Roles.js
@@ -1,6 +1,6 @@
 export default function Roles({ props }) {
   return (
-    <div className="flex space-x-4">
+    <div className="flex space-x-1 md:space-x-4">
       {props.map((item, index) => {
         return (
           <h4 key={index} className="text-xs md:text-base font-fira">
