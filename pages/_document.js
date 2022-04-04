38c625e1c1dde9c2391b4fd59edccba0f29b8449
I. Bats <bats@ibats.co>
2022-04-04 12:03:37 +0200

    remove unused font weights

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,7 @@ export default function Document() {
           crossOrigin="true"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Brygada+1918:ital,wght@0,400;0,600;0,700;1,400;1,500;1,600;1,700&family=Fira+Code&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Brygada+1918:ital,wght@0,400;1,500;1,700&family=Fira+Code&display=swap"
           rel="stylesheet"
         />{" "}
       </Head>
